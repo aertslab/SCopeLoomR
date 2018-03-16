4f436fb1f009888b2153d452fc924017d148c778
u0103631 <u0103631@gbw-s-seq07.luna.kuleuven.be>
2018-03-16 16:33:28 +0000

    v0.1.1: Added help files (man, and draft vignette)

---
+++ R/data_exports.R
@@ -0,0 +1,17 @@
+#' Documentation for the data
+
+#' @title Input dataset example 
+#' @name sce
+#' @description Small SingleCellExperiment object for examples
+#' @docType data
+#' @keywords data
+"sce"
+
+#' @title tSNE_expr
+#' @name tSNE_expr
+#' @description t-SNE for the expression matrix
+#' @docType data
+#' @keywords data
+"tSNE_expr"
+
+
